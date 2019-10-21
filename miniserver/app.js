7bfa728621e7939ca6b67cfd78f41ed6438807bc
AlbertoBermea <alberto-bermi@hotmail.com>
2019-10-20 20:28:33 -0500

    Pruebas

--- miniserver/app.js
+++ miniserver/app.js
@@ -1,36 +1,56 @@
 const express = require('express')
 
+const omdb = require('./movies.js')
+
 const app = express()
 
-const path = require('path')
-
-const publicDir = path.join(__dirname, 'public')
-
-app.use(express.static(publicDir))
+const port = process.env.PORT || 3000
 
 app.get('',function(req,res){
     res.send('Hola Mundo!')
 })
 
-app.get('/about',function(req,res){
-    res.send('Un about muy interesante')
-})
+app.get('/omdb',function(req,res){
+    if( !req.query.search ){
+        res.send({
+            error: 'Debes enviar un titulo de una pelicula o serie'    
+        })
+    }
 
-app.get('/contact',function(req,res){
-    res.send('Contact me ;)')
-})
-
-app.get('/misc',function(req,res){
-    res.send({
-        dia: 'Jueves',
-        descripcion: 'Casi viernes de ahorcar rucas'
+    omdb.omdbMovie( req.query.search, function(error,response){
+        if( error ){
+            return res.send({
+                error: error
+            })
+        }
+        if( response.seasons ){
+            var title = response.title
+            omdb.omdbSeason(response.title ,function(error,response){
+                if( error ){
+                    return res.send({
+                        error:error
+                    })
+                }
+                res.send({
+                    title: title,
+                    season: response.season,
+                    episodes: response.episodes
+                })
+            })
+        }
+        else{
+            res.send(response)
+        }
     })
+    
 })
 
 app.get('*',function(req,res){
-    res.send('Ooops!')
+    res.send({
+        error: 'Ruta no valida'
+    })
 })
 
-app.listen(3000,function(){
+app.listen(port,function(){
     console.log('Up and running!')
 })
